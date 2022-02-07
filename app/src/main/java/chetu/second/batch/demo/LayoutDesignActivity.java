8908240bc515b1f7100af8e31069039ff51e9b1c
shub008 <tyagishubham0395@gmail.com>
2022-02-07 21:45:20 +0530

    Edittext/textview/Button designed

---
+++ app/src/main/java/chetu/second/batch/demo/LayoutDesignActivity.java
@@ -0,0 +1,16 @@
+package chetu.second.batch.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LayoutDesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.design_layout);
+
+    }
+}
